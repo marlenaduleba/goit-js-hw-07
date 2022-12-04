d7cb0bfc9f9a833abc39d831c40e1673227df2e6
marlena320 <marlena_duleba@o2.pl>
2022-12-04 10:55:19 +0100

    task-02 done

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,38 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
-console.log(galleryItems);
+const gallery = document.querySelector(".gallery");
+
+makeGallery();
+
+function makeGallery() {
+  const images = galleryItems
+    .map(
+      (item) => `<a class="gallery__item" href="${item.original}">
+      <img class="gallery__image" src="${item.preview}" alt="${item.description}" />
+    </a>`
+    )
+    .join("");
+  gallery.insertAdjacentHTML("afterbegin", images);
+}
+
+gallery.addEventListener("click", selectImage);
+
+function selectImage(event) {
+  event.preventDefault();
+  if (event.target.nodeName !== "IMG") {
+    return;
+  }
+  const instance = new SimpleLightbox(".gallery a", {
+    captions: true,
+    captionSelector: "img",
+    captionType: "attr",
+    captionsData: "alt",
+    captionDelay: 250,
+  });
+  instance.open();
+
+  document.addEventListener("keydown", (e) => {
+    if (e.key === "Escape") instance.close();
+  });
+}
